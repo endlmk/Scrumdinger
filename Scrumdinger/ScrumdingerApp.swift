3a5bcfa3a5110b54ac52192e0366858ba5588241
endlmk <endlmk@users.noreply.github.com>
2022-08-30 22:51:24 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//  
+//  Created by endlmk on 2022/08/30
+//  
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
