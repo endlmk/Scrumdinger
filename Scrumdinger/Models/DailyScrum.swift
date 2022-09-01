9fc525327431f7d0efa13be7271d8f9887ed874c
endlmk <endlmk@users.noreply.github.com>
2022-09-01 21:39:51 +0900

    Creating a Card View. Section 2.

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//  
+//  Created by endlmk on 2022/09/01
+//  
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jhonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
