3edb82912cfe392a60a5164d0ef0a4f1af3ef74d
endlmk <endlmk@users.noreply.github.com>
2022-09-28 21:50:24 +0900

    Creating a navigation hierarchy, Section 2.

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//  
+//  Created by endlmk on 2022/09/28
+//  
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
