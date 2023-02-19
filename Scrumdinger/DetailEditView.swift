893f5271b8e32ced061c84c54d27a15e76887846
endlmk <1269467+endlmk@users.noreply.github.com>
2023-02-19 22:12:17 +0900

    Creating the edit view, section 4.

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -19,8 +19,9 @@ struct DetailEditView: View {
                 HStack {
                     Slider(value: $data.lengthInMinutes, in: 5...30, step: 1){ Text("Length")
                     }
+                    .accessibilityValue("\(Int(data.lengthInMinutes)) minutes")
                     Spacer()
-                    Text("\(Int(data.lengthInMinutes)) minutes")
+                    Text("\(Int(data.lengthInMinutes)) minutes").accessibilityHidden(true)
                 }
             }
             Section(header: Text("Attendees")) {
@@ -39,7 +40,7 @@ struct DetailEditView: View {
                             newAttendeeName = ""
                         }
                     }) {
-                        Image(systemName: "plus.circle.fill")
+                        Image(systemName: "plus.circle.fill").accessibilityLabel("Add Attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
